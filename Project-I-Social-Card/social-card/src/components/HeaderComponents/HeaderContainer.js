2361b52a84851a0e82d7412d421befd9bcfdd468
Your Name <you@example.com>
2019-04-22 17:08:50 -0500

    mvp project 1,app.js

--- Project-I-Social-Card/social-card/src/components/HeaderComponents/HeaderContainer.js
+++ Project-I-Social-Card/social-card/src/components/HeaderComponents/HeaderContainer.js
@@ -1,2 +1,31 @@
 import React from 'react';
 import './Header.css';
+import ImageThumbnail from './ImageThumbnail';
+import HeaderTitle from './HeaderTitle';
+import HeaderContent from './HeaderContent';
+
+
+
+const HeaderContainer=()=>{
+    return(
+        
+        <div className='headerandcontent'>
+        <div className='header'><ImageThumbnail/>
+        <HeaderTitle/>
+        
+        </div>
+       
+        <HeaderContent/>
+        
+        
+        </div>
+        
+        
+        
+        
+
+    );
+};
+
+
+export default HeaderContainer;
